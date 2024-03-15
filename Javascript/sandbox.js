490db54649ef22f8f021b219b965b7724ebbcce5
derwin23 <winnerderema@gmail.com>
2024-03-15 13:59:53 +0000

    Js String Intro

--- Javascript/sandbox.js
+++ Javascript/sandbox.js
@@ -1,14 +1,38 @@
-let age = 25;
-let year = 2019;
+// let age = 25;
+// let year = 2019;
 
-console.log(age, year);
+// console.log(age, year);
 
-age = 30;
-console.log(age);
+// age = 30;
+// console.log(age);
 
-const points = 100;
-console.log(points);
+// const points = 100;
+// console.log(points);
 
-var score = 15;
-console.log(score); 
+// var score = 15;
+// console.log(score); 
 //alert('Le score de cette partie est ' + score);
+
+// Strings
+console.log('Hello, World');
+ let email = 'example@email.com';
+console.log(email);
+
+// Strings Concatenaton
+let FirstName = 'Brad';
+let LastName = 'Pitt';
+console.log(FirstName + ' ' +LastName);
+
+// Getting Characters 
+console.log(FirstName[0]);
+
+// String Length
+console.log(FirstName.length);
+
+// String methods
+console.log(LastName.toUpperCase());
+let Lower = LastName.toLowerCase();
+console.log(Lower);
+
+let index = email.indexOf('@');
+console.log(index);
